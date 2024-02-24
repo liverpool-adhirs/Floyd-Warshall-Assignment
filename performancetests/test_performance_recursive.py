05490bbee8eed409cbd5eb5581b2f9f9b256b5fb
liverpool-adhirs <sgasoech@liverpool.ac.uk>
2024-02-24 02:05:45 -0400

    Changes made to performance tests

--- performancetests/test_performance_recursive.py
+++ performancetests/test_performance_recursive.py
@@ -5,7 +5,7 @@ This code tests the performance for the recursive code.
 import sys
 import os
 import cProfile
-import pstats
+
 
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -14,10 +14,5 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 from floydwarshallcode.floyd_recursive_code import floyd_warshall
 from floydwarshallcode.test_samples import graph_6
 
-
-if __name__ == "__main__":
-    with cProfile.Profile() as profile:
-        floyd_warshall(graph_6)
-    results = pstats.Stats(profile)
-    results.sort_stats(pstats.SortKey.TIME)
-    results.print_stats()
+#Performance testing using graph_6 as input.
+cProfile.run("floyd_warshall(graph_6)")
